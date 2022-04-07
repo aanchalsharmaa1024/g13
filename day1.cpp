a5da5bef3b124c9626acd44fd231b6ade7eda8d8
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-08 02:26:56 +0530

    to display grades

---
+++ day1.cpp
@@ -0,0 +1,8 @@
+var per=85;
+if(per>=85){
+    document.write("Your grade is A");
+} else if ( per>=60&&per<85){
+    document.write("Your grade is B");
+} else {
+    document.write("Your grade is C");
+}
