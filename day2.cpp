1d2293b089fa442b618302109b27fae663fafec6
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-08 02:29:40 +0530

    swaping two nos.

---
+++ day2.cpp
@@ -0,0 +1,9 @@
+let x= parseInt(prompt("Enter first number"));
+let y=parseInt(prompt("Enter second number"));
+
+x=x+y;
+y=x-y;
+x=x-y;
+console.log("Ater swap x is:" +x);
+console.log("After swap y is:" +y);
+
